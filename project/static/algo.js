a5b84fb2a6beaa6f9a59eca6409f9e9780ab8a32
Joel <jye5@stuy.edu>
2017-01-08 13:49:03 -0500

    some work

---
+++ project/static/algo.js
@@ -0,0 +1,27 @@
+var allCards = document.getElementById("set");
+//set should be loaded on page with all priorities (hidden)
+
+//load by lowest priority
+var getNextCard = function(){//variable is a 
+    var champ = 0;
+    for (int i = 1; i < allCards.length(); i++){
+	if (allCards.childNodes[i].priority < allCards.childNodes[champ].priority){
+	    champ = i;
+	}
+    }
+    return champ;
+    //decrement everyone's priority??? time issue
+}
+
+var updatePriorities = function(response){
+    //get currently active card
+    //use a getByID with constraint if that's a thing...
+    //just loop for now
+    for (int i = 0; i < allCards.length(); i++){
+	if (allCards.childNodes[i].current = true){
+	    allCards.childNodes[i].priority += response;
+	    return;
+	}
+    }
+    
+}
---
+++ project/utils/tableConfig.py
@@ -0,0 +1,22 @@
+#Initializes database
+import sqlite3
+
+db = sqlite3.connect("../data/main.db")
+c = db.cursor()
+
+publicSets = "CREATE TABLE PublicSets(setID INTEGER, creatorID INTEGER, setName TEXT, cardData TEXT);"
+c.execute(publicSets)
+
+users = "CREATE TABLE Users(uID INTEGER, username TEXT, hashedPass TEXT);"
+c.execute(users)
+#discrepancy b/n public/private sets need to be resolved
+
+cards = "CREATE TABLE PrivateCards(uID INTEGER, sets TEXT);"
+c.execute(cards)
+
+
+db.commit()
+db.close()
+
+#whenever someone makes a set (public or private) add a new id
+#whenever someone downloads a set, a new id is made
