19da0ff4fcde01f61d3e565a8eb4fc38f4e9db13
Joel Ye <jye5@stuy.edu>
2017-01-10 14:49:34 -0500

    nice

--- project/utils/cardDb.py
+++ project/utils/cardDb.py
@@ -13,7 +13,8 @@ def addSet(setID, creatorID, setName, cardData): #presuming card data is a strin
     c.execute(addSet)
     db.commit()
     db.close()
-    
+
+#if creator wants to take public set off public library
 def rmSet(setID):
     db = sqlite3.connect("data/main.db")
     c = db.cursor()
@@ -22,7 +23,7 @@ def rmSet(setID):
     db.commit()
     db.close()
     
-    
+        
 #PrivateCards Table -----------------------------------------------------
 def getSets(uID):
     db = sqlite3.connect("data/main.db")
@@ -33,3 +34,50 @@ def getSets(uID):
     if sel == None:
         return None
     return sel[1]
+
+def ownsSet(setID, uID): #pulled directly from Public table
+    db = sqlite3.connect("data/main.db")
+    c = db.cursor()
+    cmd = "SELECT * FROM PrivateCards WHERE uID = d;"%(uID)
+    sel = c.execute(cmd).fetchone()
+    db.close()
+    if sel == None:
+        return False
+    sets = sel.split("!!")
+    for thing in sets:
+        thing = thing.split("///")
+        if thing[0] == setID:
+            return False
+    return True
+    
+def addToLibrary(setID, creatorID, setName, cardData, uID):
+    db = sqlite3.connect("data/main.db")
+    c = db.cursor()
+    cmd = "SELECT * FROM PrivateCards WHERE uID = d;"%(uID)
+    sel = c.execute(cmd).fetchone()
+    db.close()
+    if sel == None:
+        return False
+    sets = sel.split("!!")
+    newSet = [setID, creatorID, setName, cardData]
+    "///".join(newSet)
+    sets.append(newSet)
+    "!!".join(sets)
+
+def rmFromLibrary(uID, setID):
+    db = sqlite3.connect("data/main.db")
+    c = db.cursor()
+    cmd = "SELECT * FROM PrivateCards WHERE uID = d;"%(uID)
+    sel = c.execute(cmd).fetchone()
+    db.close()
+    if sel == None:
+        return False
+    sets = sel.split("!!")
+    for thing in sets:
+        a = thing.split("///")
+        if a[0] == setID:
+            sets.remove(thing)
+            return
+    return False
+
+    
