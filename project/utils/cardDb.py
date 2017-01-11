0ae40590ef0cae98f29068acfdefa87a29ab458c
Joel <jye5@stuy.edu>
2017-01-10 19:28:39 -0500

    more work

--- project/utils/cardDb.py
+++ project/utils/cardDb.py
@@ -1,10 +1,21 @@
 import sqlite3
+#when private, add to public and private tables
 
 #PublicSets Table -----------------------------------------------------
-#exclusively public templates!
+#exclusively public templates! (what's used for search)
 
 #for ID generation, just use a md5 hash of time and userID/set name
 
+#for viewing public sets...
+def getAllSets():
+    db = sqlite3.connect("data/main.db")
+    c = db.cursor()
+    nice = "SELECT * FROM PublicSets"
+    sel = c.execute(nice)
+    ret = sel.fetchall()
+    db.close()
+    return ret
+#should probably be optimized...
 
 def addSet(setID, creatorID, setName, cardData): #presuming card data is a string for now
     db = sqlite3.connect("data/main.db")
@@ -22,10 +33,30 @@ def rmSet(setID):
     c.execute(rmSet)
     db.commit()
     db.close()
+
+def ownsPublicSet(setID, uID): #can verify from getSets
+    db = sqlite3.connect("data/main.db")
+    c = db.cursor()
+    rmSet = "SELECT * FROM PublicSets WHERE setID = '%s' AND uID = %d;"%(setID, uID)
+    sel = c.execute(rmSet).fetchone()
+    db.close()
+    if sel == None:
+        return False
+    else:
+        return True
     
-        
 #PrivateCards Table -----------------------------------------------------
 def getSets(uID):
+    db = sqlite3.connect("data/main.db")
+    c = db.cursor()
+    cmd = "SELECT * FROM PrivateCards WHERE uID = %d;"%(uID)
+    sel = c.execute(cmd).fetchone()
+    db.close()
+    if sel == None:
+        return None
+    return sel[1]
+
+def getSetData(uID, setID):
     db = sqlite3.connect("data/main.db")
     c = db.cursor()
     cmd = "SELECT * FROM PrivateCards WHERE uID = d;"%(uID)
@@ -33,8 +64,13 @@ def getSets(uID):
     db.close()
     if sel == None:
         return None
-    return sel[1]
-
+    sets = sel.split("!!")
+    for thing in sets:
+        a = thing.split("///")
+        if a[0] == setID:
+            return thing
+    return None
+    
 def ownsSet(setID, uID): #pulled directly from Public table
     db = sqlite3.connect("data/main.db")
     c = db.cursor()
@@ -80,4 +116,19 @@ def rmFromLibrary(uID, setID):
             return
     return False
 
-    
+def updateSet(uID, setID, newSetData): #upon close of session, or for editing   
+    db = sqlite3.connect("data/main.db")
+    c = db.cursor()
+    cmd = "SELECT * FROM PrivateCards WHERE uID = d;"%(uID)
+    sel = c.execute(cmd).fetchone()
+    db.close()
+    if sel == None:
+        return False
+    sets = sel.split("!!")
+    for thing in sets:
+        a = thing.split("///")
+        if a[0] == setID:
+            thing = newSetData
+            return
+    return False
+#if editing, creatorID changes, setID changes 
